43c1a5b02c596281abe1e0e5dfa3f7f3e82e9a41
mkholidkamali <mohamadkholid.kamali@gmail.com>
2022-10-02 20:48:23 +0700

    [Update] Query Param

--- routes.js
+++ routes.js
@@ -31,7 +31,14 @@ const routes = [
         method: 'GET',
         path: '/hello/{name?}',
         handler: (request, h) => {
+            const { name = "stranger" } = request.params;
             
+            const { lang } = request.query;
+
+            if (lang === 'id') {
+                return `Hai, ${name}!`;
+            }
+            return `Hello, ${name}!`;
         }
     },
     {
